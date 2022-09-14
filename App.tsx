2159aa5016952cf9076224a1b1ff85d84e25b337
Braulio <brauliolozano@outlook.com>
2022-09-14 11:05:02 -0300

    Updated Modal for registration
    Stil underconstruction

--- App.tsx
+++ App.tsx
@@ -11,7 +11,11 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/components/screens/Dashboard';
+/*import { Dashboard } from './src/components/screens/Dashboard';*/
+
+import { Register } from './src/components/screens/Register';
+
+/*import { CategorySelect } from './src/components/screens/CategorySelect'; */
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +30,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
 )}
 
---
+++ src/components/screens/Register/index.tsx
@@ -0,0 +1,90 @@
+import React, {useState} from "react";
+import { Modal } from "react-native";
+
+import { Input } from "../../Forms/Input";
+import { Button } from "../../Forms/Button";
+import { TransactionTypeButton } from "../../Forms/TransactionTypeButton";
+import { CategorySelectButton } from "../../Forms/Button/CategorySelectButton";
+
+import { CategorySelect } from "../CategorySelect";
+
+import {
+    Container,
+    Header,
+    Title,
+    Form,
+    Fields,
+    TransactionsTypes
+} from './styles';
+
+export function Register(){
+    
+    const [transactionType, setTransactionType] = useState('');
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+    const [category, setCategory] = useState({
+           key: 'category',
+           name: 'Categoria'
+    });
+
+    function handleTTSelect(type: 'up' | 'down'){
+        setTransactionType(type);
+    }
+
+    function handleOpenSelectCategoryModal(){
+        setCategoryModalOpen(true);  
+    }
+
+    function handleCloseSelectCategoryModal(){
+        setCategoryModalOpen(false);  
+    }
+
+
+    return(
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+        <Form>
+        <Fields>
+            <Input 
+                placeholder="Nome"
+            />
+            <Input 
+                placeholder="Preço"
+            />
+        <TransactionsTypes>
+            <TransactionTypeButton 
+                type="up"
+                title="Income"
+                onPress={() => handleTTSelect('up')}
+                isActive={transactionType === 'up'}
+                />
+             <TransactionTypeButton 
+                type="down"
+                title="Outcome"
+                onPress={() => handleTTSelect('down')}
+                isActive={transactionType === 'down'}
+                />    
+        </TransactionsTypes>
+
+        <CategorySelectButton 
+        title="Categoria"
+        onPress={handleOpenSelectCategoryModal}
+        />
+        </Fields>
+        <Button title="Enviar" />
+        </Form>
+
+        <Modal  visible={categoryModalOpen}>
+            <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseSelectCategoryModal}
+           />
+        </Modal>
+
+        </Container>
+
+    );
+}
